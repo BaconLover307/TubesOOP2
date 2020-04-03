caae941869f069330363c22a36027be70238070c
hengkysuryaa <hengkysurya88@gmail.com>
2020-04-03 12:41:08 +0700

    MainScreen (board.png) added in AvatarDuel

--- src/main/java/com/avatarduel/AvatarDuel-template.java
+++ src/main/java/com/avatarduel/AvatarDuel-template.java
@@ -1,19 +1,5 @@
 package com.avatarduel;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-//import javafx.application.Application;
-//import javafx.scene.Scene; 
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-//import javafx.stage.Stage;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.HBox;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -32,7 +18,6 @@ import com.avatarduel.util.CSVReader;
 public class AvatarDuel extends Application {
 /*  private static final String LAND_CSV_FILE_PATH = "card/data/land.csv";
 
-
   public void loadCards() throws IOException, URISyntaxException {
     File landCSVFile = new File(getClass().getResource(LAND_CSV_FILE_PATH).toURI());
     CSVReader landReader = new CSVReader(landCSVFile, "\t");
@@ -45,9 +30,18 @@ public class AvatarDuel extends Application {
 
   @Override
   public void start(Stage stage) {
-    MainScreen(stage);
-   
-    /*
+
+    HBox box = new HBox();
+    Scene scene = new Scene(box);
+    Image image = new Image("board.png");
+   /* Text text = new Text();
+    text.setText("Loading...");
+    text.setX(50);
+    text.setY(50);
+
+    Group root = new Group();
+    root.getChildren().add(text);
+
     Scene scene = new Scene(root, 1280, 720);
 
     stage.setTitle("Avatar Duel");
@@ -60,26 +54,6 @@ public class AvatarDuel extends Application {
     } catch (Exception e) {
       text.setText("Failed to load cards: " + e);
     } */
-  }
-
-  public void MainScreen(Stage stage) {
-    HBox hbox = new HBox();
-    Scene scene = new Scene(hbox);
-    Image image = new Image("com/avatarduel/asset/board.png");
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    BackgroundSize backgroundSize = new BackgroundSize(screenSize.getWidth(), screenSize.getHeight(), false, false, false, false);
-    BackgroundImage backgroundImage = new BackgroundImage(image,  
-    BackgroundRepeat.NO_REPEAT,  
-    BackgroundRepeat.NO_REPEAT,  
-    BackgroundPosition.DEFAULT,  
-    backgroundSize); 
-    Background background = new Background(backgroundImage);
-    hbox.setBackground(background);
-    stage.setTitle("Avatar Duel");
-    stage.setScene(scene); 
-    //stage.setFullScreen(true);
-    stage.setMaximized(true); 
-    stage.show();
   }
 
   public static void main(String[] args) {
