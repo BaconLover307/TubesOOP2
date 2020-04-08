6770be4dfa2d3e71cbeaf930de1bb8d8dcb0a66d
specialOne16 <49330357+specialOne16@users.noreply.github.com>
2020-04-08 18:26:26 +0700

    AttackPlayerEvent
    cuma di characternya (belum di player)

--- src/main/java/com/avatarduel/model/gameplay/events/AttackPlayerEvent.java
+++ src/main/java/com/avatarduel/model/gameplay/events/AttackPlayerEvent.java
@@ -1,20 +1,26 @@
 package com.avatarduel.model.gameplay.events;
+
 import com.avatarduel.model.gameplay.BaseEvent;
+import com.avatarduel.model.player.Player;
 import com.avatarduel.model.Element;
+import com.avatarduel.model.cards.card.SummonedCharacter;
+
 import java.util.ArrayList;
 
 public class AttackPlayerEvent implements BaseEvent {
+
+    public int amount;
+    public Player attacker;
+
+    public AttackPlayerEvent(int amount, Player attacker){
+        this.amount = amount;
+        this.attacker = attacker;
+    }
+
+    public interface AttackPlayerEventHandler {
+        void onAttackPlayer(AttackPlayerEvent e);
+    }
     
-    private int atk;
-
-    public AttackPlayerEvent(int atk){
-        this.atk = atk;
-    }
-
-    public int getDamage(){
-        return this.atk;
-    }
-
     public void execute(){
 
     }
