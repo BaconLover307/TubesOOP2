f5f30bb354713caa52b52bbb8d66917cac394008
hengkysuryaa <hengkysurya88@gmail.com>
2020-04-18 20:16:01 +0700

    javadoc /events done

--- src/main/java/com/avatarduel/model/gameplay/events/AttackCharacterEvent.java
+++ src/main/java/com/avatarduel/model/gameplay/events/AttackCharacterEvent.java
@@ -3,21 +3,36 @@ package com.avatarduel.model.gameplay.events;
 import com.avatarduel.model.gameplay.BaseEvent;
 import com.avatarduel.model.cards.card.SummonedCharacter;
 
+/**
+ * Kelas untuk event menyerang kartu karakter lawan
+ */
 public class AttackCharacterEvent implements BaseEvent {
 
     public SummonedCharacter fromCard;
     public SummonedCharacter toCard;
 
+    /**
+     * Membuat event baru
+     * @param fromCard kartu milik activePlayer
+     * @param toCard kartu milik kartu lawan yang ingin diserang
+     */
     public AttackCharacterEvent(SummonedCharacter fromCard, SummonedCharacter toCard){
         this.fromCard = fromCard;
         this.toCard = toCard;
     }
 
+    /**
+     * Handler untuk event
+     */
     public interface AttackCharacterEventHandler {
         void onAttackCharacter(AttackCharacterEvent e);
     }
     
+    /**
+     * Menampilkan ke layar
+     */
+    @Override
     public void execute(){
-
+        System.out.println(fromCard.getCharCard().getName() + " attack " + toCard.getCharCard().getName());
     }
 }
--- src/main/java/com/avatarduel/model/gameplay/events/SkillCardAttachedEvent.java
+++ src/main/java/com/avatarduel/model/gameplay/events/SkillCardAttachedEvent.java
@@ -4,22 +4,36 @@ import com.avatarduel.model.gameplay.BaseEvent;
 import com.avatarduel.model.cards.card.Skill;
 import com.avatarduel.model.cards.card.SummonedCharacter;
 
+/**
+ * Kelas untuk event memberikan kartu karakter suatu kartu skill
+ */
 public class SkillCardAttachedEvent implements BaseEvent {
 
     public Skill skillCard;
     public SummonedCharacter charCard;
 
+    /**
+     * Membuat event baru
+     * @param skillCard skill card yang akan dikenakan ke karakter
+     * @param charCard kartu karakter yang akan diberi skill
+     */
     public SkillCardAttachedEvent(Skill skillCard, SummonedCharacter charCard){
         this.skillCard = skillCard;
         this.charCard = charCard;
     }
 
-
+    /**
+     * Handler event
+     */
     public interface SkillCardAttachedEventHandler {
         void onSkillCardAttached(SkillCardAttachedEvent e);
     }
     
+    /**
+     * Menampilkan ke command-line
+     */
+    @Override
     public void execute() {
-
+        System.out.println(skillCard.getName() + "attached to " + charCard.getCharCard().getName());
     }
 }
