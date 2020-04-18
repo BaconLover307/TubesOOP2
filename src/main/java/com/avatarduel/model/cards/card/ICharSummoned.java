cf07de6007d0760fd40e848240cfe11ab1662097
hengkysuryaa <hengkysurya88@gmail.com>
2020-04-18 15:31:10 +0700

    javadoc /cards

--- src/main/java/com/avatarduel/model/cards/card/ICharSummoned.java
+++ src/main/java/com/avatarduel/model/cards/card/ICharSummoned.java
@@ -1,18 +1,31 @@
 package com.avatarduel.model.cards.card;
 
-// import com.avatarduel.model.cards.Skill;
-
+/**
+ * ICharSummoned merupakan interface kartu bertipe Character yang disummon
+ * @author Hengky - 13518048
+ */
 public interface ICharSummoned {
 
-    // mengubah posisi karakter dari menyerang jadi bertahan, atau sebaliknya
+    /**
+     * Mengubah posisi karakter dari menyerang jadi bertahan, atau sebaliknya
+     */
     public void rotate();
 
-    // bila dalam posisi menyerang, mengembalikan attack.
-    // bila dalam posisi bertahan, mengembalikan defense
+    /**
+     * bila dalam posisi menyerang, mengembalikan attack, bila dalam posisi bertahan, mengembalikan defense
+     * @return nilai attack/defense
+     */
     public int getPositionValue();
 
+    /**
+     * Melakukan attack ke kartu karakter lain
+     * @param target kartu karakter lain
+     */
     public void doAttack(SummonedCharacter target);
 
+    /**
+     * Menghancurkan kartu karakter yang di summon
+     */
     public void destroy();
 
 }
--- src/main/java/com/avatarduel/model/cards/card/Skill.java
+++ src/main/java/com/avatarduel/model/cards/card/Skill.java
@@ -1,19 +1,31 @@
 package com.avatarduel.model.cards.card;
 import com.avatarduel.model.Element;
 
-public abstract class Skill extends Card {
+/**
+ * Skill merupakan blueprint kartu bertipe Skill
+ * @author Hengky - 13518048
+ */
+public class Skill extends Card {
 
     // atribut
     protected int powVal;
 
-    //konstruktor
+    /**
+     * Membuat objek Skill baru
+     * @param name nama skill
+     * @param element elemen skill
+     * @param description deskripsi skill
+     * @param imagePath lokasi gambar skill
+     * @param power nilai yang dibutuhkan untuk summon skill
+     */
     public Skill(String name, Element element, String description, String imagePath, int power) {
         super(name,element,description,imagePath);
         this.powVal = power;
     }
 
-    // getter
+    /**
+     * Mengambil nilai power
+     * @return nilai power
+     */
     public int getPowVal() {return this.powVal;}
-    
-    //public abstract void useSkill(); - kelas ini hanya blueprint kartu
 }
