e750282b581ecf837fbe8b072fdbebd8559c7307
hengkysuryaa <hengkysurya88@gmail.com>
2020-04-18 18:44:50 +0700

    javadoc power event

--- src/main/java/com/avatarduel/model/gameplay/events/SpendPowerEvent.java
+++ src/main/java/com/avatarduel/model/gameplay/events/SpendPowerEvent.java
@@ -3,28 +3,39 @@ import com.avatarduel.model.gameplay.BaseEvent;
 import com.avatarduel.model.Element;
 import com.avatarduel.model.player.Player;
 
-// Sent to Power to call usePower, remembers the Element and number of Power required, remembers the sender for return address
-// returns handler with boolean, true if there was enough power of the required element
-// source: Hand
-// target: Power
-//TODO sesuaiin format
+/**
+ * Kelas untuk event mengurangi power pemain sesuai elemen
+ */
 public class SpendPowerEvent implements BaseEvent {
-
 
     public String sender;
     public Element element;
     public int amount;
 
+    /**
+     * Membuat event baru
+     * @param sender pemain yang dikenakan pengurangan
+     * @param element element yang dipakai
+     * @param amount besar element yang dipakai
+     */
     public SpendPowerEvent(String sender, Element element, int amount){
         this.sender = sender;
         this.element = element;
         this.amount = amount;
     }
     
+    /**
+     * Handler untuk event
+     */
     public interface SpendPowerEventHandler{
         public void onSpendPowerEvent(SpendPowerEvent e);
     } 
 
-    public void execute(){}
+    /**
+     * Mencetak pada command-line
+     */
+    public void execute(){
+        System.out.println(sender + element + " element reduced by " + amount);
+    }
 
 }
